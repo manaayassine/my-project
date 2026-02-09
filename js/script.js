e7e4de9937754dc5dc7a4bbaa076d4eac060d9ce
yassinemanaa27 <yassinemanaa27@gmail.com>
2026-02-09 21:20:45 +0100

    fix gif

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "../assets/cat_dance.gif";
+    catImg.src = "../assets/dance.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
