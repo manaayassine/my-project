3b1006b63b264052998445e094e276aa51c0fa28
yassinemanaa27 <yassinemanaa27@gmail.com>
2026-02-09 21:23:54 +0100

    fix gif second time

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "../assets/dance.gif";
+    catImg.src = "assets/dance.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
